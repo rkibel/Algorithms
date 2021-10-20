ecad3df05d6e55d19050097b793ca5ab7ddbbaf7
Ron Kibel <ron.kibel1@gmail.com>
2021-10-19 22:17:28 -0700

    10/19/21 changes to algorithms

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,7 +1,9 @@
+/*
 import javax.swing.tree.*;
 import java.util.*;
 public class BinarySearchTree {
     List<TreeNode> rootdata = new ArrayList<>();
-    
+    rootdata.add(new TreeNode(3));
 
 }
+*/
--- Tester.java
+++ Tester.java
@@ -1,15 +1,7 @@
 import java.util.Arrays;
 public class Tester {
     public static void main (String[] args){
-        /*int[] sample = new int[10];
-        for (int i = 0; i < sample.length; i++){
-            sample[i] = (int)(Math.random()*20);
-        }
-        QuickSort.quickSort(sample, 0, sample.length-1);
-        System.out.println(Arrays.toString(sample));
-        */
-        ArraySpiral a = new ArraySpiral();
-        int arr[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        System.out.println(a.spiral(arr).toString());
+        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 13, 42, 31, 63};
+        System.out.println(LinearSearch.linearSearch(arr, 42));
     }
 }
