7172a07c4d5c88eb2aacdc6c48d7a7f7857371b7
Ron Kibel <ron.kibel1@gmail.com>
2021-10-19 22:27:24 -0700

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 public class Tester {
     public static void main (String[] args){
-        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 13, 42, 31, 63};
+        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 13, 31, 42, 63};
         System.out.println(LinearSearch.linearSearch(arr, 42));
+        System.out.println(BinarySearch.binarySearch(arr, 42));
     }
 }
