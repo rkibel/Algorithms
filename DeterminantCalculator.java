83c21d1611b66273880a57e259b27babca00bfb2
Ron Kibel <ron.kibel1@gmail.com>
2021-11-10 11:42:19 -0800

    Update DeterminantCalculator.java

--- DeterminantCalculator.java
+++ DeterminantCalculator.java
@@ -1,26 +1,38 @@
 
 public class DeterminantCalculator {
     public static int FindDeterminant(int[][]arr){
-        if (arr.length != arr[0].length)
+        int len = arr.length;
+        int wid = arr[0].length;
+        //if it is not a square matrix throw error
+        if (len != wid)
             return Integer.MAX_VALUE;
-        if (arr.length == 2)
+
+        //2x2 matrix case: calculate determinant using known formula
+        if (len == 2)
             return arr[0][0] * arr[1][1] - arr[0][1] * arr[1][0];
         
-        int len = arr.length;
-        int[][] cofactor = new int[len-1][len-1];
+        //declare matrix of minors
+        int[][] minors = new int[len-1][wid-1];
+
+        //initialize output value
         int output = 0;
+
+        //compute determinant using cofactor expansion along the first row
         for (int j = 0; j < arr[0].length; j++){
+            //initialize matrix of minors to any [0][j] value
             int lastcol = 0;
             for (int a = 1; a < len; a++){
                 for (int b = 0; b < len; b++){
                     if (b == j)
                         continue;
-                    cofactor[a-1][lastcol] = arr[a][b];
+                    minors[a-1][lastcol] = arr[a][b];
                     lastcol++;
                 }
             }
+
+            //run recursion to find determinants of smaller matrices (if [0][j] value is 0 then ignore)
             if (arr[0][j] != 0)
-                output += arr[0][j] * Math.pow(-1, j) * FindDeterminant(cofactor);
+                output += arr[0][j] * Math.pow(-1, j) * FindDeterminant(minors);
         }
         return output;
     }
