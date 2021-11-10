adbebc98036bb1d2888da82ba3b168fc617044cf
Ron Kibel <ron.kibel1@gmail.com>
2021-11-10 11:47:07 -0800

    commenting and cleaner code

--- Tester.java
+++ Tester.java
@@ -1,7 +1,6 @@
 public class Tester {
     public static void main (String[] args){
         int[][] arr = {{3, -5, 5, 1}, {0, 0, 3, 0}, {4, -4, -5, 6}, {5, 0, 2, 4}};
-        //int[][]arr = {{0, 0}, {0, 0}};
         System.out.println(DeterminantCalculator.FindDeterminant(arr));
     }
 }
