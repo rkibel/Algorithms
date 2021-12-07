4548ec81345766a8f9116e01f40ab04dee8189e0
Ron Kibel <ron.kibel1@gmail.com>
2021-12-06 19:10:05 -0800

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,16 +1,13 @@
+import java.util.Arrays;
 public class Tester {
     public static void main (String[] args){
-        int[][] arr = {
-        {0, 2, -4}, 
-        {-8, 2, 4}, 
-        {-4, 2, 0} 
-    };
-        int eigenvalue = 4;
-        int det = DeterminantCalculator.FindDeterminantWithEigenValue(arr, eigenvalue);
-        if (det == 0)
-            System.out.print("Yes, det = ");
-        else
-            System.out.print("No, det = ");
-        System.out.println(det);
+        double[][] arr = {
+        {3, 1, 0}, 
+        {1, 0, 1}  
+        };
+        double[][] res = MatrixManipulations.orthonormalSet(arr);
+        for (double[] i : res){
+            System.out.println(Arrays.toString(i));
+        }
     }
 }
